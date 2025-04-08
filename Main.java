27be7c7c23768acb4003dcd251615bb9edb33145
derya polat <derya.polat@ug.bilkent.edu.tr>
2025-04-08 19:20:49 +0300

    Main
    added main class

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main (String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setTitle("Lost Beneath");
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
+}
